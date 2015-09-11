0dd14fb61bfee43992ec399fd547b7ef4bee4a89
Matt L <matt.larraz@gmail.com>
2015-09-10 23:18:25 -0500

    Spaces, not tabs

--- lib/bowling.ex
+++ lib/bowling.ex
@@ -1,29 +1,29 @@
 defmodule Bowling do
-	def score(frames) do
-		frames |> add_score(0)
-	end
+  def score(frames) do
+    frames |> add_score(0)
+  end
 
-	defp add_score([roll, spare, extra], total) when roll + spare == 10 do
-		[] |> add_score(total + 10 + extra)
-	end
+  defp add_score([roll, spare, extra], total) when roll + spare == 10 do
+    [] |> add_score(total + 10 + extra)
+  end
 
-	defp add_score([roll, spare|tail], total) when roll + spare == 10 do
-		tail |> add_score(total + 10 + (tail |> List.first))
-	end
+  defp add_score([roll, spare|tail], total) when roll + spare == 10 do
+    tail |> add_score(total + 10 + (tail |> List.first))
+  end
 
-	defp add_score([10, 10, 10], total) do
-		total + 30
-	end
+  defp add_score([10, 10, 10], total) do
+    total + 30
+  end
 
-	defp add_score([10|tail], total) do
-		tail |> add_score(total + 10 + (tail |> Enum.take(2) |> Enum.sum))
-	end
+  defp add_score([10|tail], total) do
+    tail |> add_score(total + 10 + (tail |> Enum.take(2) |> Enum.sum))
+  end
 
-	defp add_score([roll, spare|tail], total) do
-		tail |> add_score(total + roll + spare)
-	end
+  defp add_score([roll, spare|tail], total) do
+    tail |> add_score(total + roll + spare)
+  end
 
-	defp add_score([], total) do
-		total
-	end
+  defp add_score([], total) do
+    total
+  end
 end
